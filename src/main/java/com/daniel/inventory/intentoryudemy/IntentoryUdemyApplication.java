8332a13a5f6def83545da8c36f5e50bf4b07e259
daniel <danimc2002.dmc@gmail.com>
2024-03-05 14:46:27 +0100

    Root Commit

---
+++ src/main/java/com/daniel/inventory/intentoryudemy/IntentoryUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.inventory.intentoryudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntentoryUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntentoryUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/inventory/intentoryudemy/IntentoryUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.inventory.intentoryudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntentoryUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
